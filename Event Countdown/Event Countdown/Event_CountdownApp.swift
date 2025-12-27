8c17691ec221f7ebf47427fb67aa334fda7dcbf9
lucasnpc <lucasnpc2@outlook.com>
2025-12-27 20:23:31 -0300

    initial commit

---
+++ Event Countdown/Event Countdown/Event_CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_CountdownApp.swift
+//  Event Countdown
+//
+//  Created by Lucas Lopes on 27/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Event_CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
